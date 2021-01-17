e70e1f6807a30f92fe11f48b64ecb928c45e2210
BenCook28 <benthomascook28@gmail.com>
2021-01-17 15:38:33 -0500

    plus equals in c#

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,10 +6,10 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new double[3];
-            numbers[0] = 1.1;
-            numbers[1] = 1.1;
-            numbers[2] = 1.1;
+            var numbers = new [] {12.7, 10.3, 6.11};
+            var result = numbers[0];
+            result += numbers[1];
+            result += numbers[2];
 
             if(args.Length > 0){
                 Console.WriteLine($"Hello, {args[0]}!");
