78b501b8bf37a9a26327ec791bd5e92c643b84e9
BenCook28 <benthomascook28@gmail.com>
2021-01-17 14:34:28 -0500

    added two doubles

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,8 +6,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+
+            double x;
+            x = 34.1;
+
+            double y;
+            y = 18.3;
+
             if(args.Length > 0){
                 Console.WriteLine($"Hello, {args[0]}!");
+                Console.WriteLine(x + y);
             } 
             else{
                 Console.WriteLine("Hello");
