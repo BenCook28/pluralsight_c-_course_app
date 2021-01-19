8eb49a35cc43843368878df1fab722fddfdf2012
BenCook28 <benthomascook28@gmail.com>
2021-01-18 20:57:45 -0500

    rounding in C#

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,18 +7,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new [] {12.7, 10.3, 6.11, 4.1};
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
 
             var result = 0.0;
             var average = 0.0;
-            foreach(var number in grades){
-                result += number;
+            foreach(var grade in grades){
+                result += grade;
                 average = result / grades.Count;
             }
-            System.Console.WriteLine(result);
-            System.Console.WriteLine("average is " + average);
+            System.Console.WriteLine($"average is {average:N2}");
 
             if(args.Length > 0){
                 Console.WriteLine($"Hello, {args[0]}!");
