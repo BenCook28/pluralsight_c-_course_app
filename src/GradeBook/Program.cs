caafc4c2f425a0593ed1725a0b8cf489780a5c2c
BenCook28 <benthomascook28@gmail.com>
2021-01-20 10:37:43 -0500

    first C# class shell

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+    class Book{
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+            book.AddGrade(89.1);
+
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add(56.1);
 
@@ -16,14 +23,7 @@ namespace GradeBook
                 result += grade;
                 average = result / grades.Count;
             }
-            System.Console.WriteLine($"average is {average:N2}");
-
-            if(args.Length > 0){
-                Console.WriteLine($"Hello, {args[0]}!");
-            } 
-            else{
-                Console.WriteLine("Hello");
-            }          
+            System.Console.WriteLine($"average is {average:N2}");         
         }
     }
 }
