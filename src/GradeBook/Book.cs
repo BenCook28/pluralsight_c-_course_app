ec84a38c0bd8a8019e9563f35d4c19733b4c5073
BenCook28 <benthomascook28@gmail.com>
2021-01-21 16:52:21 -0500

    first constructor in C#

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -4,9 +4,15 @@ namespace GradeBook
 {
     class Book
     {
+        //constructor
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
         public void AddGrade(double grade){
             grades.Add(grade);
         }
-        List<double> grades;
+        List<double> grades = new List<double>();
     }
 }
