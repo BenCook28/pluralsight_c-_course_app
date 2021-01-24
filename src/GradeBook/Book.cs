727ba007e5b31849dc2aad8c89a1281be4687489
BenCook28 <benthomascook28@gmail.com>
2021-01-24 10:02:17 -0500

    encapsulation in C#

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace GradeBook
 {
@@ -11,8 +12,27 @@ namespace GradeBook
             this.name = name;
         }
 
-        public void AddGrade(double grade){
+        public void AddGrade(double grade)
+        {
             grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            var average = 0.0;
+            foreach(var grade in grades)
+            {
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
+                result += grade;
+                average = result / grades.Count;
+            }
+            Console.WriteLine($"The average is {average:N2}.");
+            Console.WriteLine($"The highest grade is {highGrade}.");
+            Console.WriteLine($"The lowest grade is {lowGrade}."); 
         }
        private List<double> grades;
        private string name;
