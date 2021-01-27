0658df7ec5425ca90be011501ad68275b0be7a7f
BenCook28 <benthomascook28@gmail.com>
2021-01-27 10:01:05 -0500

    defined event

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace GradeBook
 {
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
     public class Book
     {
         //constructor
@@ -36,11 +37,17 @@ namespace GradeBook
             if(grade <= 100 && grade >= 0)
             {
                 grades.Add(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
             } else
             {
                 throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
+
+        public GradeAddedDelegate GradeAdded;
         
         public Statistics GetStatistics()
         {
